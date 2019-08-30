dc2676102feba5c9bbd07248e1f06bed5c329026
spam-sms <54709225+spam-sms@users.noreply.github.com>
2019-08-30 14:13:05 +0300

    Update iqos.py

--- services/iqos.py
+++ services/iqos.py
@@ -4,7 +4,9 @@ from service import Service
 
 class Iqos(Service):
     def send_sms(self):
+        nam9e = ''.join(random.choice(string.ascii_letters) for _ in range(6))
+        nam8e = ''.join(random.choice(string.ascii_letters) for _ in range(6))
         self.session.post('https://ube.pmsm.org.ru/esb/iqos-reg/submission', json={
-            'data': {'firstName': self.sms_text, 'lastName': 'Иванов', 'phone': self.formatted_phone,
-                     'email': 'ivanov17@gmail.com', 'password': 'ivanov17',
-                     'passwordConfirm': 'ivanov17'}})
+            'data': {'firstName': self.sms_text, 'lastName': 'Петров', 'phone': self.formatted_phone,
+                     'email': nam9e + '@gmail.com', 'password': nam8e,
+                     'passwordConfirm': nam8e}})
