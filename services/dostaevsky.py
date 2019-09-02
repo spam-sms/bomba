845048356e9b2ed9af5ee2cbd7bc10802b768d6b
ALF-ONE <ALF-ONE@users.noreply.github.com>
2019-09-02 14:52:12 +0300

    Update dostaevsky.py

--- services/dostaevsky.py
+++ services/dostaevsky.py
@@ -4,5 +4,5 @@ from service import Service
 class Dostaevsky(Service):
     def send_sms(self):
         if self.country_code == 'ru':
-            phone = f'{self.formatted_phone[0]} {self.formatted_phone[1:4]} {self.formatted_phone[4:7]}-{self.formatted_phone[7:9]}-{self.formatted_phone[9:]}'
+            phone = f'{self.formatted_phone[0]} {self.formatted_phone[1:4]} {self.formatted_phone[4:7]}-{self.formatted_phone[7:9]}-{self.formatted_phone[9:11]}'
             self.session.post('https://dostaevsky.ru/auth/send-sms', data={'phone': phone})
