28a8b7e6b23ffa4f8ced05025f8942bf6b81337a
ALF-ONE <ALF-ONE@users.noreply.github.com>
2019-09-02 14:50:06 +0300

    Update tanuki.py

--- services/tanuki.py
+++ services/tanuki.py
@@ -6,5 +6,5 @@ class Tanuki(Service):
         phone = f'(+{self.phone_code}){self.formatted_phone[1:-1]}'
         self.session.post('https://www.tanuki.ru/api/', json={
             'header': {'version': '2.0', 'userId': '1', 'debugMode': True,
-                       'agent': {'device': 'desktop', 'version': '1'}, 'langId': '1', 'cityId': '1', 'dbgValue': ''},
-            'method': {'name': 'sendSmsCode'}, 'data': {'phone': phone, 'type': 1}})
+                       'agent': {'device': 'desktop', 'version': 'Chrome 75.0.3770.142'}, 'langId': '1', 'cityId': '1', 'dbgValue': ''},
+            'method': {'name': 'sendSmsCode'}, 'data': {'phone': phone, 'type': 2}})
