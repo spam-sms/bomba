1ae43837bbd4d38c88580d20b1530e6e26fe46e2
spam-sms <54709225+spam-sms@users.noreply.github.com>
2019-10-11 18:35:57 +0300

    Update maxidom.py

--- services/maxidom.py
+++ services/maxidom.py
@@ -1,10 +1,14 @@
 from service import Service
+import random
+import string
+
 
 
 class Maxidom(Service):
     def send_sms(self):
         if self.country_code == 'ru':
+            email = ''.join(random.choice(string.ascii_letters) for _ in range(9)) + '@mail.ru'
             phone = f'8({self.phone[1:4]}){self.phone[4:7]}-{self.phone[7:9]}-{self.phone[9:11]}'
             self.session.get('https://www.maxidom.ru/ajax/doRegister.php',
                              params={'send_code_again': 'Y', 'phone': phone,
-                                     'email': 'ivan@ivanov.com', 'code_type': 'phone'})
+                                     'email': email, 'code_type': 'phone'})
