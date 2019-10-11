52ab11847a86e80a96e031c0a483cb8f956581d9
spam-sms <54709225+spam-sms@users.noreply.github.com>
2019-10-11 18:32:53 +0300

    Update denga.py

--- services/denga.py
+++ services/denga.py
@@ -7,6 +7,7 @@ from service import Service
 class Denga(Service):
     def send_call(self):
         name = ''.join(random.choice(string.ascii_letters) for _ in range(6))
+        passwd = ''.join(random.choice(string.ascii_letters) for _ in range(9))
         self.session.get('https://online.denga.ru/admin/api/json/registration',
                          data={'phone': self.formatted_phone, 'email': name + '@gmail.com',
-                               'password': '12345678', 'passwordConfirmation': '12345678'})
+                               'password': passwd, 'passwordConfirmation': passwd})
