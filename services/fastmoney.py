8d820406302c3f8535673ec55541a657b06accc6
spam-sms <54709225+spam-sms@users.noreply.github.com>
2019-10-11 18:34:32 +0300

    Update fastmoney.py

--- services/fastmoney.py
+++ services/fastmoney.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 from service import Service
+import random
+import string
 
 
 class FastMoney(Service):
     def send_sms(self):
         if self.country_code == 'ru':
+            passwd = ''.join(random.choice(string.ascii_letters) for _ in range(9))
             self.session.post('https://fastmoney.ru/auth/registration',
                               data={'RegistrationForm[username]': '+' + self.formatted_phone,
-                                    'RegistrationForm[password]': '12345', 'RegistrationForm[confirmPassword]': '12345',
+                                    'RegistrationForm[password]': passwd, 'RegistrationForm[confirmPassword]': passwd,
                                     'yt0': 'Регистрация'})
