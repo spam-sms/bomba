32c86dde9e77ee246995f88c46bb77faa0115a00
spam-sms <54709225+spam-sms@users.noreply.github.com>
2019-10-12 22:43:29 +0300

    Delete gorzdrav.py

--- services/gorzdrav.py
+++
@@ -1,8 +0,0 @@
-from service import Service
-
-
-class Gorzdrav(Service):
-    def send_sms(self):
-        if self.country_code == 'ru':
-            phone = f'{self.formatted_phone[1:4]}) {self.formatted_phone[4:7]}-{self.formatted_phone[7:9]}-{self.formatted_phone[9:11]}'
-            self.session.post('https://gorzdrav.org/login/register/sms/send', data={'phone': phone})
