c445ca713b2b382ed9701c44386b6929c858ec51
spam-sms <54709225+spam-sms@users.noreply.github.com>
2019-10-12 22:49:50 +0300

    Delete pswallet.py

--- services/pswallet.py
+++
@@ -1,8 +0,0 @@
-from service import Service
-
-
-class PsWallet(Service):
-    def send_sms(self):
-        if self.country_code == 'ru':
-            self.session.get('https://api.pswallet.ru/system/smsCode',
-                             params={'mobile': self.formatted_phone, 'type': '0'})
