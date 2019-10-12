3e735fadcd7189130ea495b4bb59c6a3cb599f25
spam-sms <54709225+spam-sms@users.noreply.github.com>
2019-10-12 22:50:37 +0300

    Delete rutaxi.py

--- services/rutaxi.py
+++
@@ -1,7 +0,0 @@
-from service import Service
-
-
-class RuTaxi(Service):
-    def send_sms(self):
-        self.session.post('https://moscow.rutaxi.ru/ajax_keycode.html',
-                          data={'1': self.formatted_phone})
