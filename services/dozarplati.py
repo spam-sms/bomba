c61f0b6c3e17742295ad24bba48088290657369f
spam-sms <54709225+spam-sms@users.noreply.github.com>
2019-10-12 22:42:02 +0300

    Delete dozarplati.py

--- services/dozarplati.py
+++
@@ -1,9 +0,0 @@
-from service import Service
-
-
-class DoZarplati(Service):
-    def send_sms(self):
-        if self.country_code == 'ru':
-            self.session.post('https://online-api.dozarplati.com/rpc',
-                              json={'id': 1, 'jsonrpc': '2.0', 'method': 'auth.login',
-                                    'params': {'phoneNumber': self.formatted_phone}})
