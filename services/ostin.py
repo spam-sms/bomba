dbd3132f2663dd436701aa3534be2a8f52c48dc0
spam-sms <54709225+spam-sms@users.noreply.github.com>
2019-10-12 22:48:00 +0300

    Delete ostin.py

--- services/ostin.py
+++
@@ -1,9 +0,0 @@
-from service import Service
-
-
-class Ostin(Service):
-    def send_sms(self):
-        if self.country_code == 'ru':
-            phone = f' {self.formatted_phone[0]} ({self.formatted_phone[1:4]}){self.formatted_phone[4:7]}-{self.formatted_phone[7:9]}-{self.formatted_phone[9:11]}'
-            self.session.get('https://ostin.com/ru/ru/secured/myaccount/myclubcard/resultClubCard.jsp',
-                             params={'type': 'sendConfirmCode', 'phoneNumber': phone})
