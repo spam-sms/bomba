a1236eecfcba81da8ca03b288f9521ae7f95cb56
spam-sms <54709225+spam-sms@users.noreply.github.com>
2019-10-12 22:53:09 +0300

    Delete wifimetro.py

--- services/wifimetro.py
+++
@@ -1,6 +0,0 @@
-from service import Service
-
-
-class WifiMetro(Service):
-    def send_sms(self):
-        self.session.get('https://go.zazumedia.ru/auth/wait/sms/' + self.formatted_phone + '?from_reg=1')
