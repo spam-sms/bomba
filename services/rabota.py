118094f9b270e67d51947a8638202a81eedb25c0
spam-sms <54709225+spam-sms@users.noreply.github.com>
2019-10-12 22:50:05 +0300

    Delete rabota.py

--- services/rabota.py
+++
@@ -1,6 +0,0 @@
-from service import Service
-
-
-class Rabota(Service):
-    def send_sms(self):
-        self.session.post('https://www.rabota.ru/remind', data={'credential': self.formatted_phone})
