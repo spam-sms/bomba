cba31c5b9f8dd535f14a5e0907fbb85ad709a903
spam-sms <54709225+spam-sms@users.noreply.github.com>
2019-10-12 22:46:20 +0300

    Delete mvideo.py

--- services/mvideo.py
+++
@@ -1,9 +0,0 @@
-from service import Service
-
-
-class MVideo(Service):
-    def send_sms(self):
-        if self.country_code == 'ru':
-            phone = f'{self.phone[1:4]}-{self.phone[4:-1]}'
-            self.session.post('https://www.mvideo.ru/internal-rest-api/common/atg/rest/actors/VerificationActor/getCode',
-                              params={'pageName': 'registerPrivateUserPhoneVerification'}, data={'phone': phone})
