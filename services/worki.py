dc64fbc3a9427a61657925ddb194489a8e6c5b7e
spam-sms <54709225+spam-sms@users.noreply.github.com>
2019-10-12 22:53:27 +0300

    Delete worki.py

--- services/worki.py
+++
@@ -1,6 +0,0 @@
-from service import Service
-
-
-class Worki(Service):
-    def send_sms(self):
-        self.session.post('https://api.iconjob.co/api/web/v1/verification_code', data={'phone': self.formatted_phone})
