d154b2f87922d7adfdee73117c27d0f6798f6295
spam-sms <54709225+spam-sms@users.noreply.github.com>
2019-10-12 22:45:25 +0300

    Update mistercat.py

--- services/mistercat.py
+++ services/mistercat.py
@@ -3,8 +3,7 @@ from service import Service
 
 class MisterCat(Service):
     def send_sms(self):
-        if self.country_code == 'ua':
-            self.session.post('https://mistercat.com.ua/index.php',
-                              params={'option': 'com_ksenmart', 'view': 'profile', 'task': 'profile.sms_auth',
-                                      'tmpl': 'ksenmart'},
-                              data={'phone': self.formatted_phone, 'type': 'send'})
+        self.session.post('https://mistercat.com.ua/index.php',
+                          params={'option': 'com_ksenmart', 'view': 'profile', 'task': 'profile.sms_auth',
+                                  'tmpl': 'ksenmart'},
+                          data={'phone': self.formatted_phone, 'type': 'send'})
