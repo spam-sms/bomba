66cccb8c5e0fcf66e6ce135776b5b2b898a30ca9
spam-sms <54709225+spam-sms@users.noreply.github.com>
2019-10-12 22:54:00 +0300

    Delete s7.py

--- services/s7.py
+++
@@ -1,8 +0,0 @@
-from service import Service
-
-
-class S7(Service):
-    def send_sms(self):
-        self.session.get('https://www.s7.ru/dotCMS/priority/ajaxEnrollment',
-                         params={'dispatch': 'shortEnrollmentByPhone', 'mobilePhone.countryCode': self.phone_code,
-                                 'mobilePhone.areaCode': self.phone[1:4], 'mobilePhone.localNumber': self.phone[4:-1]})
