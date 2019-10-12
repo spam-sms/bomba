c81c484ec09594c9ee34f65f9d43ca50251311d8
spam-sms <54709225+spam-sms@users.noreply.github.com>
2019-10-12 22:52:36 +0300

    Delete uramobil.py

--- services/uramobil.py
+++
@@ -1,6 +0,0 @@
-from service import Service
-
-
-class UraMobil(Service):
-    def send_sms(self):
-        self.session.post('https://service.uramobil.ru/profile/smstoken', data={'PhoneNumber': self.formatted_phone})
