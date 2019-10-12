201d5d850685f72d8353d24ab910ed3e9ba0d7f8
spam-sms <54709225+spam-sms@users.noreply.github.com>
2019-10-12 21:58:43 +0300

    Delete mcdonalds.py

--- services/mcdonalds.py
+++
@@ -1,8 +0,0 @@
-from service import Service
-
-
-class McDonalds(Service):
-    def send_sms(self):
-    phone = f'8({self.formatted_phone[1:4]}){self.formatted_phone[4:7]}-{self.formatted_phone[7:9]}-{self.formatted_phone[9:11]}'
-        self.session.post('http://smsgorod.ru/sendsms.php',
-                          data={'number': phone})
