e5aff9b756e3c030f48b417c977ac31bbf5861ee
spam-sms <54709225+spam-sms@users.noreply.github.com>
2019-10-12 22:47:29 +0300

    Update optima.py

--- services/optima.py
+++ services/optima.py
@@ -2,6 +2,5 @@ from service import Service
 
 
 class Optima(Service):
-    def send_sms(self):
-        if self.country_code == 'ua':
-            self.session.post('https://online.optima.taxi/user/get-code', data={'phone': self.formatted_phone})
+    def send_sms(self):    
+        self.session.post('https://online.optima.taxi/user/get-code', data={'phone': self.formatted_phone})
