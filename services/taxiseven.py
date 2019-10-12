d426360603d2b4c297b7a1062433afcb7789d9c4
spam-sms <54709225+spam-sms@users.noreply.github.com>
2019-10-12 22:51:59 +0300

    Delete taxiseven.py

--- services/taxiseven.py
+++
@@ -1,6 +0,0 @@
-from service import Service
-
-
-class TaxiSeven(Service):
-    def send_sms(self):
-        self.session.post('http://taxiseven.ru/auth/register', data={'phone': self.formatted_phone})
