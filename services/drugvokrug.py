967a2fe4990c4716be2e1fb64c5341c3e7d53316
spam-sms <54709225+spam-sms@users.noreply.github.com>
2019-10-12 22:42:21 +0300

    Delete drugvokrug.py

--- services/drugvokrug.py
+++
@@ -1,6 +0,0 @@
-from service import Service
-
-
-class DrugVokrug(Service):
-    def send_sms(self):
-        self.session.post('https://drugvokrug.ru/siteActions/processSms.htm', data={'cell': self.formatted_phone})
