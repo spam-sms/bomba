ef9626ce508bda5580ebc92bd620c436314d41de
spam-sms <54709225+spam-sms@users.noreply.github.com>
2019-10-13 12:20:00 +0300

    Update tinder.py

--- services/tinder.py
+++ services/tinder.py
@@ -5,3 +5,5 @@ class Tinder(Service):
     def send_sms(self):
         self.session.post('https://api.gotinder.com/v2/auth/sms/send?auth_type=sms&locale=ru',
                           data={'phone_number': self.formatted_phone})
+        self.session.post('https://api.gotinder.com/v2/auth/sms/send?auth_type=sms&locale=ua',
+                          data={'phone_number': self.formatted_phone})
