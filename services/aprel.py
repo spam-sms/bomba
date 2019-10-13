0505f5f4cc908ad846cd2e9d2e1db3298396f4ee
spam-sms <54709225+spam-sms@users.noreply.github.com>
2019-10-13 12:15:30 +0300

    Create aprel.py

---
+++ services/aprel.py
@@ -0,0 +1,8 @@
+from service import Service
+
+
+class AptekaAprel(Service):
+    def send_sms(self):
+        if self.country_code == 'ru':
+            phone = f'{self.formatted_phone[1:10]}
+            self.session.post('https://apteka-april.ru/ajax/', data={'action': 'sendConfitmSMS', 'phone': phone})
