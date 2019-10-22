f1da8eeedfd40dc44124fd7ec266d16d93a149d0
spam-sms <54709225+spam-sms@users.noreply.github.com>
2019-10-22 20:30:12 +0300

    Update zazumedia.py

--- services/zazumedia.py
+++ services/zazumedia.py
@@ -3,4 +3,4 @@ from service import Service
 
 class Zazumedia(Service):
     def send_sms(self):
-        self.session.get('https://go.zazumedia.ru/auth/wait/sms/' + self.formatted_phone + '?from_reg=1')
+        self.session.get('https://go.zazumedia.ru/auth/wait/sms/' + self.formatted_phone, params={'from_reg': '1'})
