6506100e29facef9816aa03300c9fadf1077fa04
spam-sms <54709225+spam-sms@users.noreply.github.com>
2019-10-22 20:12:56 +0300

    Update aprel.py

--- services/aprel.py
+++ services/aprel.py
@@ -5,4 +5,4 @@ class AptekaAprel(Service):
     def send_sms(self):
         if self.country_code == 'ru':
             phone = f'{self.formatted_phone[1:10]}'
-            self.session.post('https://apteka-april.ru/ajax/', data={'action': 'sendConfitmSMS', 'phone': phone})
+            self.session.post('https://apteka-april.ru/ajax/', data={'action': 'sendConfirmSMS', 'phone': phone})
